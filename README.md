338cde39f5797cb08e7ccbf9e2b629b61be89c1c
Adam Pritchard <pritchard.adam@gmail.com>
2019-09-26 15:00:37 -0400

    First attempt at packaging

---
+++ README.md
@@ -0,0 +1,3 @@
+# transifexlib
+
+This is a library used by Psiphon projects to help with pulling translations from Transifex.
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='transifexlib',
+    version='1.0.0',
+    description='Transifex helper library for Psiphon projects',
+    author='Psiphon Inc.',
+    url='https://github.com/Psiphon-Inc/transifexlib',
+    license='GPLv3',
+    py_modules=['transifexlib'],
+    packages=setuptools.find_packages(),
+)
